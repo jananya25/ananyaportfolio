6a06b82b2e157d5d972e60d06c33377cd4575de1
Anaj404 <jananya09@gmail.com>
2025-06-29 11:49:26 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path";
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@/assets": path.resolve(__dirname, "src/assets"),
-      "@/components": path.resolve(__dirname, "src/components"),
-      "@/data": path.resolve(__dirname, "src/data"),
-      "@/hooks": path.resolve(__dirname, "src/hooks"),
-      "@/lib": path.resolve(__dirname, "src/lib"),
-      "@/pages": path.resolve(__dirname, "src/pages"),
-      "@/types": path.resolve(__dirname, "src/types"),
-      "@/utils": path.resolve(__dirname, "src/utils"),
-    }
-  }
-});
+      "@/assets": resolve(__dirname, "src/assets"),
+      "@/components": resolve(__dirname, "src/components"),
+      "@/data": resolve(__dirname, "src/data"),
+      "@/hooks": resolve(__dirname, "src/hooks"),
+      "@/lib": resolve(__dirname, "src/lib"),
+      "@/pages": resolve(__dirname, "src/pages"),
+      "@/types": resolve(__dirname, "src/types"),
+      "@/utils": resolve(__dirname, "src/utils"),
+    },
+  },
+})
